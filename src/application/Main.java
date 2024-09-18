359171f10bd39f14528557fdc2dc9d0409597ad3
Felipe Moreira <felipe0365@gmail.com>
2024-09-18 10:42:05 -0300

    First class: Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Position p1 = new Position(3, 5);
+
+        System.out.println(p1   );
 
     }
 }
